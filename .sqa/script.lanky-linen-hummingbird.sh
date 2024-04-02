26217f886bdb43c84d81e7a67c37eadc5ec35f79
Pablo Orviz <orviz@ifca.unican.es>
2024-04-02 14:45:26 +0200

    Add JePL file structure

---
+++ .sqa/script.lanky-linen-hummingbird.sh
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd gitlab.com/dtgeo/workflow-registry &&
+    echo 'Here we will execute COMPSs'
+)
