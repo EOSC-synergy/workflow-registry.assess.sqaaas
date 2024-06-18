6fab0456f81a44bcd1296167243bddcd18caf4fd
Pablo Orviz <orviz@ifca.unican.es>
2024-06-18 13:02:44 +0200

    Add JePL file structure

---
+++ .sqa/script.wimpy-ecru-corgi.sh
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd gitlab.com/dtgeo/workflow-registry/DTC52/WF5201/ST520101 &&
+    mdl --json .
+)
